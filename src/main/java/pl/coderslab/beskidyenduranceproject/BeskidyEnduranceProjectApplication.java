5a0966a0e870ec0a1a4c0c57b2bd23c26c21bcae
ppuczka <p.puczka@gmail.com>
2018-11-19 20:00:30 +0100

    Spring Boot Project

---
+++ src/main/java/pl/coderslab/beskidyenduranceproject/BeskidyEnduranceProjectApplication.java
@@ -0,0 +1,12 @@
+package pl.coderslab.beskidyenduranceproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeskidyEnduranceProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeskidyEnduranceProjectApplication.class, args);
+    }
+}
