08ef5a8895530cfe40f1ce22b3dcd96f556d4bd7
ppuczka <p.puczka@gmail.com>
2019-01-16 22:11:45 +0100

    first test

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,3 +6,4 @@ REPLACE INTO `role` VALUES (2,'USER');
 
 INSERT INTO `user` (`user_id`, `active`, `created`, `email`, `first_name`, `last_name`, `password`, `points`)
 VALUES (1, 1, '2018-12-02 11:11:04', 'p.puczka@gmail.com', 'Przemek', 'Puczka', '$2a$10$f4oILZUHHr2hNrE/FZFpzOPcNrs5IZ8vishpl8Ep35SdD/x2i63RC', 0);
+
---
+++ src/test/java/pl/coderslab/beskidyenduranceproject/controller/RankControllerTest.java
@@ -0,0 +1,12 @@
+package pl.coderslab.beskidyenduranceproject.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RankControllerTest {
+
+    @Test
+    public void viewMyRank() {
+    }
+}
